4b433859e59438f0a60a8f6a0a03c2350eeeb6cf
nytln <nytln97@gmail.com>
2022-02-14 20:03:29 +0200

    for in for

--- src/com/company/Main10.java
+++ src/com/company/Main10.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Main10 {
     public static void main(String[] args){
-        
+
+        for(int j = 1; j < 11; j++) {
+            System.out.println();
+            for (int i = 1; i < 11; i++) {
+                System.out.print(+i*j + "\t");
+            }
+        }
     }
 }
