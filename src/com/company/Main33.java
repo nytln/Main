3dcb303a15097e80230c9fc6ddca0d0d837d42cc
nytln <nytln97@gmail.com>
2022-03-09 19:45:22 +0200

    09.03.2022

--- src/com/company/Main33.java
+++ src/com/company/Main33.java
@@ -2,6 +2,15 @@ package com.company;
 
 public class Main33 {
     public static void main(String[] args){
-
+        int [] arr = {5, 6, 7, 8};
+        try{
+//            int num1;
+//            num1 = 100/0;
+            System.out.println(arr[2]);
+        }catch (Exception e){
+            System.out.println("Some error" + e);
+        }finally{
+            System.out.println("hi");
+        }
     }
 }
